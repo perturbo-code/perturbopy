8403f8d3cd468a8a80434f45f09242d619fd78cc
Ivan <ivan.maliyov@gmail.com>
2022-02-24 16:24:32 -0800

    updated path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src/perturbopy/'))
+sys.path.insert(0, os.path.abspath('../../src/'))
 
 
 # -- Project information -----------------------------------------------------
