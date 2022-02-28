1766968bddfe23f823f5a1dabb9f51ac130dbe71
Ivan Maliyov <ivan.maliyov@gmail.com>
2022-02-27 19:18:31 -0800

    added scripts for NERSC

--- tests/test_scripts/env_setup_examples.sh
+++ tests/test_scripts/env_setup_examples.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-#machine="HSW"
+# Check if the machine value is specified as as the command line argument
+if [[ "$1" == "" ]]; then
+   machine="KNL"
+else
+   machine=$1
+fi
 
-machine="KNL"
+# Check if the $SLURM_NNODES variable is defined
+if [[ "$SLURM_NNODES" == "" ]]; then
+   nodes=1
+else
+   nodes=$SLURM_NNODES
+fi
 
 if [[ "$machine" == "HSW" ]]; then
 
@@ -11,7 +21,7 @@ if [[ "$machine" == "HSW" ]]; then
 
    mpi_per_node=8
    omp_threads=8
-   threads_total=8
+   cpus_per_task=8
 
 elif [[ "$machine" == "KNL" ]]; then
 
@@ -20,21 +30,21 @@ elif [[ "$machine" == "KNL" ]]; then
 
    mpi_per_node=4
    omp_threads=64
-   threads_total=68
+   cpus_per_task=68
 
 else
    echo "WRONG machine value" $machine
    exit
 fi
 
+# Setup the OpenMP environment
 export OMP_NUM_THREADS=$omp_threads
 export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
-nodes="$SLURM_NNODES"
-mpi_tasks=`echo $mpi_tasks $nodes | awk '{print $1*$2}'`
+mpi_tasks=`echo $mpi_per_node $nodes | awk '{print $1*$2}'`
 
-export PERTURBO_RUN="srun -n $mpi_per_node -c $threads_total --cpu_bind=cores $PERTURBO -npools $mpi_per_node"
+export PERTURBO_RUN="srun -n $mpi_tasks -c $cpus_per_task --cpu_bind=cores $PERTURBO -npools $mpi_tasks"
 
 echo "PERTURBO_RUN COMMAND:"
 echo $PERTURBO_RUN
