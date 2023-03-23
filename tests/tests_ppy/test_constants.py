e61c74815d90c7885f8d5f51ca37f8d30572eb59
Shaelyn <shaelyniyer@gmail.com>
2023-03-23 11:58:53 -0500

    flake8 fixes to test suite

--- tests/tests_ppy/test_constants.py
+++ tests/tests_ppy/test_constants.py
@@ -1,63 +1,59 @@
-import numpy as np
-import os
 import pytest
 import math
 
 import perturbopy.postproc as ppy
 
-test_dict = {'bohr':['bohr', 'a.u', 'atomic units', 'au'], 'angstrom':['angstrom, a'], 'm':['m', 'meter'] }
+test_dict = {'bohr': ['bohr', 'a.u', 'atomic units', 'au'], 'angstrom': ['angstrom, a'], 'm': ['m', 'meter']}
 test_vals = {'bohr': (1, 0), 'angstrom': (0.529177249, 0), 'm': (5.29177249, -11)}
 
-@pytest.mark.parametrize("test_input, expected", [
-	('m',-3), ('M', 6), ('mu', -6), ('Y', 24), ('',0)
-	])
 
+@pytest.mark.parametrize("test_input, expected", [
+                        ('m', -3), ('M', 6), ('mu', -6), ('Y', 24), ('', 0)
+])
 def test_prefix_exp(test_input, expected):
-	assert(ppy.constants.prefix_exp(test_input) == expected)
-	assert(ppy.constants.prefix_negative_exp(test_input) == -1 * expected)
+   assert(ppy.constants.prefix_exp(test_input) == expected)
+   assert(ppy.constants.prefix_negative_exp(test_input) == -1 * expected)
+
 
 @pytest.mark.parametrize("test_input, expected", [
-	('a.u',('','bohr')), ('A.U',('','bohr')), ('Bohr',('','bohr')),
-	('nm',('n','m'))
-	])
+                        ('a.u', ('', 'bohr')), ('A.U', ('', 'bohr')), ('Bohr', ('', 'bohr')),
+                        ('nm', ('n', 'm'))
+])
 def test_find_prefix_and_base_units(test_input, expected):
-	assert(ppy.constants.find_prefix_and_base_units(test_input, test_dict) == expected)
+   assert(ppy.constants.find_prefix_and_base_units(test_input, test_dict) == expected)
+
 
 @pytest.mark.parametrize("test_input, expected", [
-	('a.u','bohr'), ('A.U','bohr'), ('Bohr','bohr'),
-	('nm','nm')
-	])
+                        ('a.u', 'bohr'), ('A.U', 'bohr'), ('Bohr', 'bohr'),
+                        ('nm', 'nm')
+])
 def test_standardize_units_name(test_input, expected):
-	assert(ppy.constants.standardize_units_name(test_input, test_dict) == expected)
+   assert(ppy.constants.standardize_units_name(test_input, test_dict) == expected)
 
-@pytest.mark.parametrize("test_input1, test_input2, expected", [ 
-	('a.u', 'bohr', 1), ('a.u', 'nm', 5.29177249e-2),
-	('cm', 'fm', 1e13), ('fm', 'cm', 1e-13),
-	('a.u', 'bohr', 1), ('a.u', 'nm', 5.29177249e-2),
-	('cm', 'fm', 1e13), ('fm', 'cm', 1e-13)
-	])
+
+@pytest.mark.parametrize("test_input1, test_input2, expected", [
+                        ('a.u', 'bohr', 1), ('a.u', 'nm', 5.29177249e-2),
+                        ('cm', 'fm', 1e13), ('fm', 'cm', 1e-13),
+                        ('a.u', 'bohr', 1), ('a.u', 'nm', 5.29177249e-2),
+                        ('cm', 'fm', 1e13), ('fm', 'cm', 1e-13)
+])
 def test_conversion_factor(test_input1, test_input2, expected):
-	assert(math.isclose(ppy.constants.conversion_factor(test_input1, test_input2, test_dict, test_vals), expected))
+   assert(math.isclose(ppy.constants.conversion_factor(test_input1, test_input2, test_dict, test_vals), expected))
 
-# @pytest.mark.parametrize("test_input1, test_input2, expected", [ 
-	
-# 	])
-# def test_energy_conversion_factor(test_input1, test_input2, expected):
-# 	assert(math.isclose(ppy.constants.conversion_factor(test_input1, test_input2), expected))
 
-@pytest.mark.parametrize("test_input1, test_input2, expected", [ 
-	('a.u', 'bohr', 1), ('a.u', 'nm', 5.29177249e-2),
-	('cm', 'fm', 1e13), ('fm', 'cm', 1e-13),
-	('a.u', 'bohr', 1), ('a.u', 'nm', 5.29177249e-2),
-	('cm', 'fm', 1e13), ('fm', 'cm', 1e-13)
-	])
+@pytest.mark.parametrize("test_input1, test_input2, expected", [
+                        ('a.u', 'bohr', 1), ('a.u', 'nm', 5.29177249e-2),
+                        ('cm', 'fm', 1e13), ('fm', 'cm', 1e-13),
+                        ('a.u', 'bohr', 1), ('a.u', 'nm', 5.29177249e-2),
+                        ('cm', 'fm', 1e13), ('fm', 'cm', 1e-13)
+])
 def test_length_conversion_factor(test_input1, test_input2, expected):
-	assert(math.isclose(ppy.constants.length_conversion_factor(test_input1, test_input2), expected))
+   assert(math.isclose(ppy.constants.length_conversion_factor(test_input1, test_input2), expected))
 
-@pytest.mark.parametrize("test_input, expected", [ 
-	('ev*fs', 0.6582119569), ('atomic', 1)
-	])
-   
+
+@pytest.mark.parametrize("test_input, expected", [
+                        ('ev*fs', 0.6582119569), ('atomic', 1)
+])
 def test_hbar(test_input, expected):
-	print(ppy.constants.hbar(test_input))
-	assert(math.isclose(ppy.constants.hbar(test_input), expected))
+   print(ppy.constants.hbar(test_input))
+   assert(math.isclose(ppy.constants.hbar(test_input), expected))
--- tests/tests_ppy/test_lattice.py
+++ tests/tests_ppy/test_lattice.py
@@ -1,106 +1,99 @@
 import numpy as np
-import os
 import pytest
 import math
 
 import perturbopy.postproc as ppy
 
+
 @pytest.mark.parametrize("test_input, expected", [
 
-	([3,3,2], np.array([[3],[3],[2]])),
-	(np.array([3,3,2]), np.array([[3],[3],[2]])),
-	([[1,1,1],[2,2,2],[3,3,3]], np.array([[1,1,1],[2,2,2],[3,3,3]])),
-	([[1,1,1],[2,2,2],[3,3,3],[4,4,4]], np.array([[1,2,3,4],[1,2,3,4],[1,2,3,4]])),
-	([[1,2,3,4],[1,2,3,4],[1,2,3,4]], np.array([[1,2,3,4],[1,2,3,4],[1,2,3,4]])),
-	([[1,1],[2,2],[3,3]], np.array([[1,1],[2,2],[3,3]])),
-	([[1,2,3],[1,2,3]], np.array([[1,1],[2,2],[3,3]]))
+                        ([3, 3, 2], np.array([[3], [3], [2]])),
+                        (np.array([3, 3, 2]), np.array([[3], [3], [2]])),
+                        ([[1, 1, 1], [2, 2, 2], [3, 3, 3]], np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3]])),
+                        ([[1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 4]], np.array([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]])),
+                        ([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]], np.array([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]])),
+                        ([[1, 1], [2, 2], [3, 3]], np.array([[1, 1], [2, 2], [3, 3]])),
+                        ([[1, 2, 3], [1, 2, 3]], np.array([[1, 1], [2, 2], [3, 3]]))
 
-	])
-
+])
 def test_reshape_points(test_input, expected):
-	test_result = ppy.lattice.reshape_points(test_input)
+   test_result = ppy.lattice.reshape_points(test_input)
 
-	assert(np.all(test_result == expected))
-	assert(isinstance(test_result, np.ndarray))
+   assert(np.all(test_result == expected))
+   assert(isinstance(test_result, np.ndarray))
 
-	if len(np.shape(test_input)) == 1:
-		assert(np.shape(test_result) == (3,1))
-	elif np.shape(test_input)[0] == 3:
-		assert(np.shape(test_result) == (3, np.shape(test_input)[1]))
-	elif np.shape(test_input)[1] == 3:
-		assert(np.shape(test_result) == (3, np.shape(test_input)[0]))
+   if len(np.shape(test_input)) == 1:
+     assert(np.shape(test_result) == (3, 1))
+   elif np.shape(test_input)[0] == 3:
+     assert(np.shape(test_result) == (3, np.shape(test_input)[1]))
+   elif np.shape(test_input)[1] == 3:
+     assert(np.shape(test_result) == (3, np.shape(test_input)[0]))
+
 
 @pytest.mark.parametrize("test_points_cart, test_points_cryst, test_lat, test_recip_lat", [
+                        ([[0, 0, 0.5, 0.25, 0.25, 0.375], [0, 0.5, 0.5, 0.75, 0.625, 0.75], [0, 0.5, 0.5, 0.5, 0.625, 0.375]],
+                         [[0, 0, 0.5, 0.5, 0.25, 0.75], [0, 1, 0.5, 1, 1, 0.75], [0, 0, 0.5, 0, 0.25, 0.]],
+                         np.array([[0.5, 0.5, 0.0], [0.0, 0.5, 0.5], [0.5, 0.0, 0.5]]).T,
+                         np.array([[1.0, 1.0, -1.0], [-1.0, 1.0, 1.0], [1.0, -1.0, 1.0]]).T)
 
-	# [0.5, 0.5, 0], [0, 0.5, 0.5], [0.5, 0, 0.5]
-
-	([[0,0,0.5,0.25,0.25,0.375], [0,0.5,0.5,0.75,0.625,0.75], [0,0.5,0.5,0.5,0.625,0.375]],
-	 [[0, 0, 0.5, 0.5,  0.25, 0.75], [0, 1, 0.5, 1,   1,   0.75], [0,   0,   0.5, 0,   0.25,0.]],
-	 np.array([[0.5, 0.5, 0.0], [0.0, 0.5, 0.5], [0.5, 0.0, 0.5]]).T,
-	 np.array([[1.0, 1.0, -1.0], [-1.0, 1.0, 1.0], [1.0, -1.0, 1.0]]).T)
-
-	])
-
+])
 def test_cryst_to_cart(test_points_cart, test_points_cryst, test_lat, test_recip_lat):
-	print(test_points_cart)
-	print(ppy.lattice.cryst_to_cart(test_points_cart, test_lat, test_recip_lat, real_space=True, forward=True))
-	assert(np.allclose(ppy.lattice.cryst_to_cart(test_points_cryst, test_lat, test_recip_lat, real_space=True, forward=True), test_points_cart))
-	assert(np.allclose(ppy.lattice.cryst_to_cart(test_points_cart, test_lat, test_recip_lat, real_space=True, forward=False),  test_points_cryst))
+   print(test_points_cart)
+   print(ppy.lattice.cryst_to_cart(test_points_cart, test_lat, test_recip_lat, real_space=True, forward=True))
+   assert(np.allclose(ppy.lattice.cryst_to_cart(test_points_cryst, test_lat, test_recip_lat, real_space=True, forward=True), test_points_cart))
+   assert(np.allclose(ppy.lattice.cryst_to_cart(test_points_cart, test_lat, test_recip_lat, real_space=True, forward=False), test_points_cryst))
 
-	# assert(np.allclose(ppy.lattice.cryst_to_cart(test_points_cart, test_lat, test_recip_lat, real_space=False, forward=True), test_points_cryst))
-	# assert(np.allclose(ppy.lattice.cryst_to_cart(test_points_cryst, test_lat, test_recip_lat, real_space=False, forward=False),  test_points_cart))
 
 @pytest.mark.parametrize("test_input1, test_input2, expected", [
 
-	([3,3,1],[3,3,1], 0),
-	([[3],[2],[1]],[3,2,1], 0),
-	([[3,2,1]],[3,2,1], 0),
-	([1,1,1],[0,2,0], math.sqrt(3)),
-	([[1,2,3],[4,5,6]], [1,2,3,], [0, math.sqrt(27)]),
-	([[1,4],[2,5],[3,6]], [1,2,3,], [0, math.sqrt(27)]),
-	([[1,2,3],[4,5,6]], [[2,3,4],[5,6,7]], [math.sqrt(3), math.sqrt(3)]),
-	([[1,4],[2,5],[3,6]], [[2,3,4],[5,6,7]], [math.sqrt(3), math.sqrt(3)])
+                        ([3, 3, 1], [3, 3, 1], 0),
+                        ([[3], [2], [1]], [3, 2, 1], 0),
+                        ([[3, 2, 1]], [3, 2, 1], 0),
+                        ([1, 1, 1], [0, 2, 0], math.sqrt(3)),
+                        ([[1, 2, 3], [4, 5, 6]], [1, 2, 3], [0, math.sqrt(27)]),
+                        ([[1, 4], [2, 5], [3, 6]], [1, 2, 3], [0, math.sqrt(27)]),
+                        ([[1, 2, 3], [4, 5, 6]], [[2, 3, 4], [5, 6, 7]], [math.sqrt(3), math.sqrt(3)]),
+                        ([[1, 4], [2, 5], [3, 6]], [[2, 3, 4], [5, 6, 7]], [math.sqrt(3), math.sqrt(3)])
 
-	])
-
+])
 def test_compute_distances(test_input1, test_input2, expected):
-	assert(np.all(np.isclose(ppy.lattice.compute_distances(test_input1, test_input2), expected)))
-	assert(np.all(np.isclose(ppy.lattice.compute_distances(test_input2, test_input1), expected)))
+   assert(np.all(np.isclose(ppy.lattice.compute_distances(test_input1, test_input2), expected)))
+   assert(np.all(np.isclose(ppy.lattice.compute_distances(test_input2, test_input1), expected)))
 
 
 @pytest.mark.parametrize("test_point, test_points_array, atol, expected", [
-	([1,1,1],[[1,1,1],[1,1,1],[1,1,1]], None, [0,1,2]),
-	([1,1,1],[[1,1,1],[1,1,1],[.96,.96,.96]], None, [0,1,2]),
-	([1,1,1],[[1,1,1],[1,1,1],[.9,.9,.9]], None, None),
-	([1,1,1],[[1,1,1],[1,1,1],[.9,.9,.9]], 0.2, [0,1,2]),
-	([[1],[1],[1]],[[1,1,1],[1,1,1],[.9,.9,.9]], 0.2, [0,1,2]),
-	([1,1,1],[[1,1,1],[1,1,1],[1,.96,.96]], None, [0])
-	])
-
+                        ([1, 1, 1], [[1, 1, 1], [1, 1, 1], [1, 1, 1]], None, [0, 1, 2]),
+                        ([1, 1, 1], [[1, 1, 1], [1, 1, 1], [.96, 0.96, 0.96]], None, [0, 1, 2]),
+                        ([1, 1, 1], [[1, 1, 1], [1, 1, 1], [.9, 0.9, 0.9]], None, None),
+                        ([1, 1, 1], [[1, 1, 1], [1, 1, 1], [.9, 0.9, 0.9]], 0.2, [0, 1, 2]),
+                        ([[1], [1], [1]], [[1, 1, 1], [1, 1, 1], [.9, 0.9, 0.9]], 0.2, [0, 1, 2]),
+                        ([1, 1, 1], [[1, 1, 1], [1, 1, 1], [1, 0.96, 0.96]], None, [0])
+])
 def test_where(test_point, test_points_array, atol, expected):
-	if atol is None:
-		assert(np.all(ppy.lattice.where(test_point, test_points_array) == expected))
-	else:
-		assert(np.all(ppy.lattice.where(test_point, test_points_array, atol=atol) == expected))
+   if atol is None:
+     assert(np.all(ppy.lattice.where(test_point, test_points_array) == expected))
+   else:
+     assert(np.all(ppy.lattice.where(test_point, test_points_array, atol=atol) == expected))
+
 
 @pytest.mark.parametrize("test_point, test_points_array, test_path_array, atol, expected", [
-	([1,2,0],[[1,1,1],[1,5,6],[1,2,0], [3,3,3]], [1,2,3,4], None, 3),
-	([1,2,0],[[1,1,1],[1,5,6],[1.02,2.01,0.04], [3,3,3]], [1,2,3,4], None, 3),
-	([1,2,0],[[1,1,1],[1,2,0],[1,2,0], [3,3,3]], [0.1,0.2,0.3,0.4], None, [0.2,0.3]),
-	([1,2,0],[[1,1,1],[1,5,6],[1,2.1,0], [3,3,3]], [1,2,3,4], None, None),
-	([1,2,0],[[1,1,1],[1,5,6],[1,2.1,0], [3,3,3]], [1,2,3,4], 0.12, 3),
-	([1,2,0],[[1,1,1],[1,2,0],[1,2.1,0], [3,3,3]], [1,2,3,4], 0.12, 2)
-	])
-
+                        ([1, 2, 0], [[1, 1, 1], [1, 5, 6], [1, 2, 0], [3, 3, 3]], [1, 2, 3, 4], None, 3),
+                        ([1, 2, 0], [[1, 1, 1], [1, 5, 6], [1.02, 2.01, 0.04], [3, 3, 3]], [1, 2, 3, 4], None, 3),
+                        ([1, 2, 0], [[1, 1, 1], [1, 2, 0], [1, 2, 0], [3, 3, 3]], [0.1, 0.2, 0.3, 0.4], None, [0.2, 0.3]),
+                        ([1, 2, 0], [[1, 1, 1], [1, 5, 6], [1, 2.1, 0], [3, 3, 3]], [1, 2, 3, 4], None, None),
+                        ([1, 2, 0], [[1, 1, 1], [1, 5, 6], [1, 2.1, 0], [3, 3, 3]], [1, 2, 3, 4], 0.12, 3),
+                        ([1, 2, 0], [[1, 1, 1], [1, 2, 0], [1, 2.1, 0], [3, 3, 3]], [1, 2, 3, 4], 0.12, 2)
+])
 def test_point_to_path(test_point, test_points_array, test_path_array, atol, expected):
-	if atol is None:
-		assert(np.all(ppy.lattice.point_to_path(test_point, test_points_array, test_path_array) == expected))
-	else:
-		assert(np.all(ppy.lattice.point_to_path(test_point, test_points_array, test_path_array, atol=atol) == expected))
+   if atol is None:
+     assert(np.all(ppy.lattice.point_to_path(test_point, test_points_array, test_path_array) == expected))
+   else:
+     assert(np.all(ppy.lattice.point_to_path(test_point, test_points_array, test_path_array, atol=atol) == expected))
+
 
 @pytest.mark.parametrize("test_path, test_points_array, test_path_array, atol, expected", [
-	(3,[[1,1,1],[1,5,6],[1,2,0], [3,3,3]], [1,2,3,4], None, [1,2,0]),
-	])
+                        (3, [[1, 1, 1], [1, 5, 6], [1, 2, 0], [3, 3, 3]], [1, 2, 3, 4], None, [1, 2, 0]),
+])
 def test_path_to_point(test_path, test_points_array, test_path_array, atol, expected):
-    print(ppy.lattice.path_to_point(test_path, test_points_array, test_path_array))
-    assert(np.all(np.isclose(ppy.lattice.path_to_point(test_path, test_points_array, test_path_array), expected)))
+   print(ppy.lattice.path_to_point(test_path, test_points_array, test_path_array))
+   assert(np.all(np.isclose(ppy.lattice.path_to_point(test_path, test_points_array, test_path_array), expected)))
