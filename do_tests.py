a87bbec3bd85ecee48b6a1385d313f7e0c6db8a6
Sergei Kliavinek <klyavinekss@gmail.com>
2023-08-10 09:05:46 -0700

    change naming for tests running functions and files

--- do_tests.py
+++
@@ -1,3 +0,0 @@
-from perturbopy import run_tests
-
-run_tests.do_tests(["-s", "--source_folder", './tests_f90', '--run_qe2pert'])
---
+++ run_tests.py
@@ -0,0 +1,3 @@
+from perturbopy import tests_use
+
+tests_use.do_tests(["-s", "--source_folder", './tests_f90', '--run_qe2pert'])
