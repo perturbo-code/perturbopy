75df09366060713c84a4aa31e1550d230e576491
Sergei Kliavinek <klyavinekss@gmail.com>
2023-10-20 12:50:12 -0700

    add producting of error files in case of the fail of coma comparisons

--- src/perturbopy/test_utils/compare_data/yaml.py
+++ src/perturbopy/test_utils/compare_data/yaml.py
@@ -4,6 +4,7 @@ This module contains functions to compare YAML files
 """
 from numbers import Number
 import numpy as np
+import yaml
 import perturbopy.test_utils.compare_data.h5 as ch5
 from perturbopy.test_utils.run_test.run_utils import get_tol
 from perturbopy.io_utils.io import open_yaml
@@ -16,18 +17,22 @@ def equal_scalar(scalar1, scalar2, key, ig_n_tol):
     Parameters
     ----------
     scalar1 : numpy.dtype
-       first  scalar
+        first  scalar
     scalar2 : numpy.dtype
-       second scalar
+        second scalar
     key : str
-       key associated with this scalar
+        key associated with this scalar
     ig_n_tol : dict
-       dictionary of ignore keywords and tolerances needed to make comparison on values
+        dictionary of ignore keywords and tolerances needed to make comparison on values
 
     Returns
     -------
     equal_value : bool
-       boolean specifying if both scalars contain the same values
+        boolean specifying if both scalars contain the same values
+    diff_str : str
+        string which contains the information about the calculation error
+    output_res_val : str
+        here for consistency, just a copy of the diff_str
 
     """
     # check that scalar1 and scalar2 are Numbers
@@ -46,12 +51,14 @@ def equal_scalar(scalar1, scalar2, key, ig_n_tol):
 
     if np.abs(scalar1) > 1e-10:
         rdiff = np.abs((scalar2 - scalar1) / scalar1)
-        diff_str = f'{diff:.1e}, {rdiff*100:.1e}%, {scalar1 = }, {scalar2 = }'
+        diff_str = f'{diff:.1e}, {rdiff*100:.1e}%, computed={scalar1}, reference={scalar2}'
 
     else:
         diff_str = f'{diff:.1e}'
 
-    return equal_value, diff_str
+    output_res_val = f'{diff_str}'
+
+    return equal_value, diff_str, output_res_val
 
 
 def equal_list(list1, list2, key, ig_n_tol, path):
@@ -61,21 +68,28 @@ def equal_list(list1, list2, key, ig_n_tol, path):
     Parameters
     ----------
     list1 : list
-       first  list
+        first  list
     list2 : list
-       second list
+        second list
     key: str
-       A key for the tolerance. If this key is not specified in the tolerance dict,
-       a default tolerance will be applied
+        A key for the tolerance. If this key is not specified in the tolerance dict,
+        a default tolerance will be applied
     ig_n_tol : dict
-       dictionary of ignore keywords and tolerances needed to make comparison on values
+        dictionary of ignore keywords and tolerances needed to make comparison on values
     path : str
-       pseudo path to this item being compared
+        pseudo path to this item being compared
 
     Returns
     -------
     equal_vlaues : bool
-       boolean specifying if both lists are equivalent
+        boolean specifying if both lists are equivalent
+    diff : str
+        string which contains the information about the number of
+        failed tests
+    output_res_list : list
+        list with testing results. Either the item from the
+        produced yaml-file will be saved, or it will be saved
+        with the additional string FAIL
 
     """
     # check that list1 and list2 are lists
@@ -86,18 +100,10 @@ def equal_list(list1, list2, key, ig_n_tol, path):
     assert len(list1) == len(list2), errmsg
     indices = range(len(list1))
 
-    # check if lists can be converted to ndarray
-    if (all(isinstance(x, Number) for x in list1) and all(isinstance(x, Number) for x in list2)):
-
-        # compare lists as nparrays for speed up
-        return ch5.equal_ndarray(np.array(list1),
-                                 np.array(list2),
-                                 key,
-                                 ig_n_tol)
-
     # a list of bool values
     equal_per_item = []
 
+    output_res_list = []
     for item1, item2, index in zip(list1, list2, indices):
         errmsg = ('list1/2 values'
                   'are not of the same type')
@@ -106,13 +112,13 @@ def equal_list(list1, list2, key, ig_n_tol, path):
         # pseudo path to current item being compared
         item_path = (f'{path}.list[{index}]')
         if isinstance(item1, dict):
-            equal_value, diff = equal_dict(item1, item2, ig_n_tol, item_path)
+            equal_value, diff, output_res = equal_dict(item1, item2, ig_n_tol, item_path)
 
         elif isinstance(item1, list):
-            equal_value, diff = equal_list(item1, item2, key, ig_n_tol, item_path)
+            equal_value, diff, output_res = equal_list(item1, item2, key, ig_n_tol, item_path)
 
         elif isinstance(item1, Number):
-            equal_value, diff = equal_scalar(item1, item2, key, ig_n_tol)
+            equal_value, diff, output_res = equal_scalar(item1, item2, key, ig_n_tol)
 
         elif isinstance(item1, str):
             equal_value = item1 == item2
@@ -131,6 +137,9 @@ def equal_list(list1, list2, key, ig_n_tol, path):
         if not equal_value:
             print(f'\n !!! discrepancy found at {item_path}')
             print(f' difference: {diff}')
+            output_res_list.append(['!!!!FAIL!!!!', output_res])
+        else:
+            output_res_list.append(f'{item2}')
 
     # equal dicts produce list of only bool=True
     nitems = len(equal_per_item)
@@ -143,8 +152,8 @@ def equal_list(list1, list2, key, ig_n_tol, path):
     else:
         diff = f'among {nitems} elements, {nitems - ncompared} failed comparison'
 
-    return equal_values, diff
-
+    return equal_values, diff, output_res_list
+    
 
 def equal_dict(dict1, dict2, ig_n_tol, path):
     """
@@ -154,18 +163,25 @@ def equal_dict(dict1, dict2, ig_n_tol, path):
     Parameters
     ----------
     dict1 : dict
-       first  dictionary
+        first  dictionary
     dict2 : dict
-       second dictionary
+        second dictionary
     ig_n_tol : dict
-       dictionary of ignore keywords and tolerances needed to make comparison on values
+        dictionary of ignore keywords and tolerances needed to make comparison on values
     path : str
-       pseudo path to this item being compared
+        pseudo path to this item being compared
 
     Returns
     -------
     equal_vlaues : bool
-       boolean specifying if both dicts contain the same keys and values
+        boolean specifying if both dicts contain the same keys and values
+    diff : str
+        string which contains the information about the number of
+        failed tests
+    output_res_dict : dict
+        list with testing results. Either the item from the
+        produced yaml-file will be saved, or it will be saved
+        with the additional string FAIL
 
     """
     # check that dict1 and dict2 are dictionaries
@@ -187,7 +203,7 @@ def equal_dict(dict1, dict2, ig_n_tol, path):
 
     # a list of bool values
     equal_per_key = []
-
+    output_res_dict = {}
     for key in keys:
         errmsg = (f'dict1/2 values associated with key:{key} '
                   f'are not of the same type')
@@ -196,13 +212,13 @@ def equal_dict(dict1, dict2, ig_n_tol, path):
         # pseudo path to current item being compared
         key_path = (f'{path}.{key}')
         if isinstance(dict1[key], dict):
-            equal_value, diff = equal_dict(dict1[key], dict2[key], ig_n_tol, key_path)
+            equal_value, diff, output_res = equal_dict(dict1[key], dict2[key], ig_n_tol, key_path)
 
         elif isinstance(dict1[key], list):
-            equal_value, diff = equal_list(dict1[key], dict2[key], key, ig_n_tol, key_path)
+            equal_value, diff, output_res = equal_list(dict1[key], dict2[key], key, ig_n_tol, key_path)
 
         elif isinstance(dict1[key], Number):
-            equal_value, diff = equal_scalar(dict1[key], dict2[key], key, ig_n_tol)
+            equal_value, diff, output_res = equal_scalar(dict1[key], dict2[key], key, ig_n_tol)
 
         elif isinstance(dict1[key], str):
             equal_value = dict1[key] == dict2[key]
@@ -222,6 +238,9 @@ def equal_dict(dict1, dict2, ig_n_tol, path):
         if not equal_value:
             print(f'\n !!! discrepancy found at {key_path}')
             print(f' difference: {diff}')
+            output_res_dict[key] = ["# !!!!FAIL!!!!", output_res]
+        else:
+            output_res_dict[key] = [dict2[key]]
 
     # equal dicts produce list of only bool=True
     nitems = len(equal_per_key)
@@ -234,7 +253,7 @@ def equal_dict(dict1, dict2, ig_n_tol, path):
     else:
         diff = f'among {nitems} elements, {nitems - ncompared} failed comparison'
 
-    return equal_values, diff
+    return equal_values, diff, output_res_dict
 
 
 def equal_values(file1, file2, ig_n_tol):
@@ -245,16 +264,23 @@ def equal_values(file1, file2, ig_n_tol):
     Parameters
     ----------
     file1 : str
-       first  YAML file name
+        first  YAML file name
     file2 : str
-       second YAML file name
+        second YAML file name
     ig_n_tol : dict
-       dictionary of keywords and tolerances needed to make comparison on files
+        dictionary of keywords and tolerances needed to make comparison on files
 
     Returns
     -------
     equal_vlaues : bool
-       boolean specifying if both YAML files contain the same keys and values
+        boolean specifying if both YAML files contain the same keys and values
+    diff : str
+        string which contains the information about the number of
+        failed tests
+    
+    .. note::
+        Also, the file {name_of_file}_errors_file.yml will be generated in case of the fail
+        of some comparisons.
 
     """
     yaml1_dict = open_yaml(file1)
@@ -273,4 +299,12 @@ def equal_values(file1, file2, ig_n_tol):
         assert len(yaml1_dict) > 0, errmsg
         assert len(yaml2_dict) > 0, errmsg
 
-    return equal_dict(yaml1_dict, yaml2_dict, ig_n_tol, 'top of yaml')
+    equal_values, diff, output_res_dict = equal_dict(yaml1_dict, yaml2_dict, ig_n_tol, 'top of yaml')
+
+    if not equal_values:
+        file_name = f"{file2[:file2.find('.yml')]}_errors_file.yml"
+
+        with open(file_name, 'w', encoding='utf-8') as yaml_file:
+            yaml.dump(output_res_dict, yaml_file, allow_unicode=True)
+
+    return equal_values, diff
