b5c512aeba46af0234d10e14bce3bd0a3d5b11ca
Sergei Kliavinek <klyavinekss@gmail.com>
2024-05-09 12:43:52 -0700

    delete print

--- src/perturbopy/tests_use.py
+++ src/perturbopy/tests_use.py
@@ -5,7 +5,6 @@ from perturbopy import testing_code
 
 def do_tests(testing_args):
     dir_path = os.path.dirname(testing_code.__file__)
-    print(dir_path)
     testing_args.insert(0, dir_path)
     result = pytest.main(testing_args)
     return result
