0ba46c0e48435bdef5854eca2b1c0d365a7791d9
Laurie Tan <latan@caltech.edu>
2024-11-27 10:58:04 -0800

    Fixed positional argument error in spins plotting function

--- src/perturbopy/postproc/calc_modes/spins.py
+++ src/perturbopy/postproc/calc_modes/spins.py
@@ -81,7 +81,7 @@ class Spins(CalcMode):
 
         values = {key: 1 - val for key, val in self.spins.items()}
 
-        ax = plot_vals_on_bands(ax, self.kpt.path, self.bands, self.bands.units, values=values, log, label=r'$|<n|\Sigma_z|n>|$', **kwargs)
+        ax = plot_vals_on_bands(ax, self.kpt.path, self.bands, self.bands.units, values=values, log=log, label=r'$|<n|\Sigma_z|n>|$', **kwargs)
 
         if show_kpoint_labels:
             ax = plot_recip_pt_labels(ax, self.kpt.labels, self.kpt.points, self.kpt.path)
