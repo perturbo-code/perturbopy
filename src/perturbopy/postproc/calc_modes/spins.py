e8a4abd99fa1e1370cac0478ef59734c85dbb684
Laurie Tan <165857560+ltan01@users.noreply.github.com>
2024-12-09 08:02:07 -0800

    Simplify attribute names for imsigma_spin calc mode and fix typo (#82)
    * Simplified attribute names for imsigma_spin calc mode to match imsigma calc mode. Also corrected typo in spins yaml file
    
    * Fixed flake8 error

--- src/perturbopy/postproc/calc_modes/spins.py
+++ src/perturbopy/postproc/calc_modes/spins.py
@@ -45,7 +45,7 @@ class Spins(CalcMode):
         energy_units = self._pert_dict['spins'].pop('band units')
 
         spins_dict = self._pert_dict['spins'].pop('band index (spins)')
-        spin_units = self._pert_dict['spins'].pop('<n|Sigma_z|n> units')
+        spin_units = self._pert_dict['spins'].pop('<n|sigma_z|n> units')
 
         self.kpt = RecipPtDB.from_lattice(kpoint, kpoint_units, self.lat, self.recip_lat, kpath, kpath_units)
         self.bands = UnitsDict.from_dict(energies_dict, energy_units)
@@ -53,7 +53,7 @@ class Spins(CalcMode):
 
     def plot_spins(self, ax, kpoint_idx=0, show_kpoint_labels=True, log=True, **kwargs):
         """
-        Method to plot the <n|Sigma_z|n> values over the band structure.
+        Method to plot the <n|sigma_z|n> values over the band structure.
 
         Parameters
         ----------
@@ -61,7 +61,7 @@ class Spins(CalcMode):
            Axis on which to plot the bands.
         
         kpoint_idx : int, optional
-           Index of the k-point to plot the <n|Sigma_z|n> values for. <n|Sigma_z|n> elements will be plotted along k-points, at this k-point
+           Index of the k-point to plot the <n|sigma_z|n> values for. <n|sigma_z|n> elements will be plotted along k-points, at this k-point
            By default, it will be the first k-point.
 
         show_kpoint_labels : bool, optional
@@ -81,7 +81,7 @@ class Spins(CalcMode):
 
         values = {key: 1 - val for key, val in self.spins.items()}
 
-        ax = plot_vals_on_bands(ax, self.kpt.path, self.bands, self.bands.units, values=values, log=log, label=r'$|<n|\Sigma_z|n>|$', **kwargs)
+        ax = plot_vals_on_bands(ax, self.kpt.path, self.bands, self.bands.units, values=values, log=log, label=r'$|\langle n|\sigma_z|n\rangle |$', **kwargs)
 
         if show_kpoint_labels:
             ax = plot_recip_pt_labels(ax, self.kpt.labels, self.kpt.points, self.kpt.path)
