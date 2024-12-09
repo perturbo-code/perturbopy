1a5f883a81349b9864ef8a3db5b683aba136190d
Laurie Tan <latan@caltech.edu>
2024-12-08 17:14:35 -0800

    Fixed flake8 error

--- src/perturbopy/postproc/calc_modes/spins.py
+++ src/perturbopy/postproc/calc_modes/spins.py
@@ -81,7 +81,7 @@ class Spins(CalcMode):
 
         values = {key: 1 - val for key, val in self.spins.items()}
 
-        ax = plot_vals_on_bands(ax, self.kpt.path, self.bands, self.bands.units, values=values, log=log, label=r'$|\langle n | \sigma_z | n \rangle |$', **kwargs)
+        ax = plot_vals_on_bands(ax, self.kpt.path, self.bands, self.bands.units, values=values, log=log, label=r'$|\langle n|\sigma_z|n\rangle |$', **kwargs)
 
         if show_kpoint_labels:
             ax = plot_recip_pt_labels(ax, self.kpt.labels, self.kpt.points, self.kpt.path)
