3557c21e438e2dfc6b18bc46f9797b7f4a36ae56
Sergei Kliavinek <klyavinekss@gmail.com>
2025-03-07 13:28:25 -0800

    Update version to 0.7.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='perturbopy',
-    version='0.7.0',
+    version='0.7.1',
     description="Suite of Python scripts for Perturbo testing and postprocessing",
     long_description=long_description,
     long_description_content_type="text/markdown",
